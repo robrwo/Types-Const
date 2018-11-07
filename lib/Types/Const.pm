1f211672d2aeb89e638df14094f6ad03df53731c
Robert Rothenberg <rrwo@cpan.org>
2018-11-07 22:30:58 +0000

    Add support for parameterized types

--- lib/Types/Const.pm
+++ lib/Types/Const.pm
@@ -12,16 +12,19 @@ use Type::Library
    -declare => qw/ ConstArrayRef ConstHashRef /;
 
 use Const::Fast ();
+use List::Util 1.33 ();
+use Type::Coercion;
 use Type::Tiny;
 use Type::Utils -all;
-use Types::Standard -types;
+use Types::Standard qw/ -types is_ArrayRef is_HashRef is_Ref /;
+use Types::TypeTiny ();
 
 # RECOMMEND PREREQ: Ref::Util::XS
 # RECOMMEND PREREQ: Type::Tiny::XS
 
 use namespace::autoclean 0.28;
 
-our $VERSION = 'v0.1.2';
+our $VERSION = 'v0.2.0';
 
 =head1 SYNOPSIS
 
@@ -49,15 +52,22 @@ A read-only array reference.
 
 declare "ConstArrayRef",
   as ArrayRef,
-  where   { Internals::SvREADONLY(@$_) },
+  where   \&__is_readonly,
   message {
     return ArrayRef->get_message($_) unless ArrayRef->check($_);
     return "$_ is not readonly";
-  };
+  },
+  constraint_generator => \&__arrayref_constraint_generator,
+  coercion_generator   => \&__coercion_generator;
 
 coerce "ConstArrayRef",
   from ArrayRef,
-  via { Const::Fast::_make_readonly( $_ => 0 ); return $_; };
+  via \&__coerce_constant;
+
+declare_coercion "ConstFromArrayRef",
+  to_type ConstArrayRef[Any],
+  from ArrayRef[Any],
+  via \&__coerce_constant;
 
 =type C<ConstHashRef>
 
@@ -67,21 +77,112 @@ A read-only hash reference.
 
 declare "ConstHashRef",
   as HashRef,
-  where   { Internals::SvREADONLY(%$_) },
+  where   \&__is_readonly,
   message {
     return HashRef->get_message($_) unless HashRef->check($_);
     return "$_ is not readonly";
-  };
+  },
+  constraint_generator => \&__hashref_constraint_generator,
+  coercion_generator   => \&__coercion_generator;
 
 coerce "ConstHashRef",
   from HashRef,
-  via { Const::Fast::_make_readonly( $_ => 0 ); return $_; };
+  via \&__coerce_constant;
+
+sub __coerce_constant {
+    my $value = @_ ? $_[0] : $_;
+    Const::Fast::_make_readonly( $value => 0 );
+    return $value;
+}
+
+sub __arrayref_constraint_generator {
+    return ConstArrayRef unless @_;
+
+    my $param = shift;
+    Types::TypeTiny::TypeTiny->check($param)
+        or _croak("Parameter to ConstArrayRef[`a] expected to be a type constraint; got $param");
+
+    _croak("Only one parameter to ConstArrayRef[`a] expected; got @{[ 1 + @_ ]}.")
+        if @_;
+
+    my $psub = ArrayRef->parameterize($param)->constraint;
+
+    return sub {
+        return $psub->($_) && __is_readonly($_);
+    };
+
+}
+
+sub __hashref_constraint_generator {
+    return ConstHashRef unless @_;
+
+    my $param = shift;
+    Types::TypeTiny::TypeTiny->check($param)
+        or _croak("Parameter to ConstHashRef[`a] expected to be a type constraint; got $param");
+
+    my $psub = HashRef->parameterize($param)->constraint;
+
+    return sub {
+        return psub->($_) && __is_readonly($_);
+    };
+
+}
+
+sub __is_readonly {
+    if ( is_ArrayRef($_) ) {
+        return Internals::SvREADONLY(@$_)
+          && List::Util::all { __is_readonly($_) } @$_;
+    }
+    elsif ( is_HashRef($_) ) {
+        &Internals::hv_clear_placeholders($_);
+        return Internals::SvREADONLY(%$_)
+          && List::Util::all { __is_readonly($_) } values %$_;
+    }
+    elsif ( is_Ref($_) ) {
+        return Internals::SvREADONLY($$_);
+    }
+    else {
+        return Internals::SvREADONLY($_);
+    }
+}
+
+sub __coercion_generator {
+    my ( $parent, $child, $param ) = @_;
+
+    my $coercion = Type::Coercion->new( type_constraint => $child );
+
+    if ( $param->has_coercion ) {
+
+        my $coercable_item = $param->coercion->_source_type_union;
+
+        $coercion->add_type_coercions(
+            $parent => sub {
+                my $value = @_ ? $_[0] : $_;
+                my @new;
+                for my $item (@$value) {
+                    return $value unless $coercable_item->check($item);
+                    push @new, $param->coerce($item);
+                }
+                return __coerce_constant(\@new);
+            },
+        );
+
+    }
+    else {
+
+        # If there's no coercion for the parameter, then just coerce
+        # to a constant.
+
+        $coercion->add_type_coercions(
+            $parent => \&__coerce_constant,
+        );
+
+    }
+
+    return $coercion;
+}
 
 1;
-
-=head1 KNOWN ISSUES
-
-Parameterized types, e.g. C<ConstArrayRef[Int]> are not yet supported.
 
 =head1 SEE ALSO
 
---
+++ t/11-parameterized.t
@@ -0,0 +1,36 @@
+#!perl
+
+use Test::Most;
+
+use Const::Fast;
+use Types::Const -types;
+use Types::Standard -types;
+
+subtest 'parameterize empty' => sub {
+
+    ok my $type = ConstArrayRef [], 'parameterize';
+    is $type->display_name => ConstArrayRef->display_name, 'display_name'
+
+};
+
+subtest 'parameterize Int' => sub {
+
+    ok my $type = ConstArrayRef [Int], 'parameterize';
+    is $type->display_name => "ConstArrayRef[Int]", 'display_name';
+
+    const my @empty => ();
+    const my @ints  => ( 1 .. 3 );
+    const my @strs  => qw/ a b c /;
+
+    ok $type->check( \@empty ), 'check';
+    ok $type->check( \@ints ),  'check';
+    ok !$type->check( \@strs ), 'check fails (not int)';
+
+    my @vals = ( 1 .. 3 );
+    ok !$type->check( \@vals ), 'check fails (not read-only)';
+
+    ok my $cvals = $type->coerce( \@vals ), 'coerce';
+
+};
+
+done_testing;
